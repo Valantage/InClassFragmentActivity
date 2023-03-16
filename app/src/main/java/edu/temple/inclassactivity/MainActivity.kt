68e5c05e06d766c3ecdb8e42af54979376e3f787
Luigi Mazzocchi <tun50126@temple.edu>
2023-03-16 15:03:26 -0400

    Created an Instance of ImageDisplayFragment

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Attach an instance of ImageDisplayFragment using factory method
-
+        val frag = ImageDisplayFragment.newInstance(imageArray)
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView, frag).commit()
     }
 }
